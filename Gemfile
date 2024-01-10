98a5ac7901a054f0f84ecc66100bef2652deaab7
pcbennett108 <pcbennett42@gmail.com>
2024-01-10 14:32:37 -0700

    Add setup for simplecov, webmock, vcr

--- Gemfile
+++ Gemfile
@@ -53,3 +53,7 @@ group :development do
   # gem "spring"
 end
 
+group :test do
+  gem "webmock"
+  gem "vcr"
+end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,4 +1,7 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
+require 'simplecov'
+SimpleCov.start
+
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
@@ -62,6 +65,12 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+end
+
+VCR.configure do |config|
+  config.cassette_library_dir = "spec/fixtures/vcr_cassettes"
+  config.hook_into :webmock
+  config.configure_rspec_metadata!
 end
 
 Shoulda::Matchers.configure do |config|
