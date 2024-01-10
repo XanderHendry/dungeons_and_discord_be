4e0d520ddcae41f7c9881108a771767de329737d
pcbennett108 <pcbennett42@gmail.com>
2024-01-10 15:00:40 -0700

    Add test for get_charclesses

---
+++ spec/facades/charclass_facade_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe CharclassFacade do
+  let (:facade) { CharclassFacade }
+
+  describe "#get_charclasses", :vcr do
+    it "returns an array of Charclass objects" do
+      response = facade.get_charclasses
+
+      expect(response).to be_an(Array)
+      expect(response.first).to be_a(Charclass)
+    end
+  end
+
+  describe "#get_charclasses", :vcr do
+    it "returns a Charclass object" do
+      response = facade.get_charclass("barbarian")
+
+      expect(response).to be_an(Array)
+      expect(response.first).to be_a(Charclass)
+    end
+  end
+end
