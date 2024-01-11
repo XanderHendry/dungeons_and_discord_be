d61b9c9980c1822a11898d7c00053419e00cc2ae
pcbennett108 <pcbennett42@gmail.com>
2024-01-11 11:42:28 -0700

    setup factorybot and add test for character serializer

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,5 +1,10 @@
 FactoryBot.define do
   factory :character do
-    name { "#{Faker::Games::DnD.name} Market" }
+    user_id { Faker::Number.number(digits: 1) }
+    name { Faker::Games::DnD.name }
+    char_class { Faker::Games::DnD.klass }
+    race { Faker::Games::DnD.race }
+    alignment { Faker::Games::DnD.alignment }
+    background { Faker::Games::DnD.background }
   end
 end
