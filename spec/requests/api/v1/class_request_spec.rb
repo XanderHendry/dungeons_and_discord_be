320e43c91bb09127c17683716c1ca667855bbd9a
pcbennett108 <pcbennett42@gmail.com>
2024-01-11 16:06:44 -0700

    Add request test for character

--- spec/requests/api/v1/class_request_spec.rb
+++ spec/requests/api/v1/class_request_spec.rb
@@ -1,11 +1,8 @@
-require 'rails_helper'
+# require 'rails_helper'
 
-describe "class requests for API" do
-  it "sends a list of classes", :vcr do
-    # Charclass.create(index: "test", name: "testing", url: "testing.com")
-    # Charclass.create(index: "test3", name: "testing2", url: "testing2.com")
-    # Charclass.create(index: "test2", name: "testing3", url: "testing3.com")
-    # Charclass.create(index: "test4", name: "testing4", url: "testing4.com")
+# describe "class requests for API" do
+#   it "sends a list of classes", :vcr do
+
 
 #     get '/api/v1/charclasses/barbarian'
 #     expect(response).to be_successful
@@ -14,7 +11,7 @@ describe "class requests for API" do
 #     expect(charclass[:data][:attributes][:name]).to eq("Barbarian")
 #   end
 
-#   it 'lets you create models based on character creation choices' do
+#   # xit 'lets you create models based on character creation choices' do
     
-#   end
+#   # end
 # end
