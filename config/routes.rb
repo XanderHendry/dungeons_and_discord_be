dbeb9bb9058e6e998a87d7ec6fd359b0c830938a
Joseph Lee <jhjlee702@gmail.com>
2024-04-01 15:48:27 -0600

    remove charclasses show/index route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      # resources :charclasses, only: [:index, :show]
       get '/charclasses/:index', to: 'charclasses#show'
       post '/charclasses/:char_id/add_prof', to: 'charclasses#add_prof'
       post '/charclasses/:char_id/add_items', to: 'charclasses#add_items'
